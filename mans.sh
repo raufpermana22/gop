a634625393c6f338f79f267a400bed76dada0230
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-05-27 17:35:24 +0900

    Update mans.sh

--- mans.sh
+++ mans.sh
@@ -1,6 +1,6 @@
-apt-get update -y
-apt install screen -y
-apt install -y lynx
+sudo apt-get update -y
+sudo apt install screen -y
+sudo apt install -y lynx
 lynx -dont_wrap_pre -dump "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz" > lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.hk.nicehash.com:3353  --user 3KExLjD5yz5w1vuvByBSpzUq1W8bjw3FhC.GPU-MT_RECEH-$(echo $(shuf -i 1-99 -n 1))--vapers --ethstratum ETHPROXY
