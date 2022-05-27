10f5f909e6ca188a2f2dd402c55579717e37d126
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-05-27 19:10:44 +0900

    Update dog.sh

--- dog.sh
+++ dog.sh
@@ -5,6 +5,6 @@ tar -xf lolMiner_v1.48_Lin64.tar.gz &&
 while [ 1 ]; do
 cd 1.48/
 ./lolMiner --coin AUTO192_7 --pool equihash192.sea.mine.zpool.ca --port 2192 --user DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS --ethstratum ETHPROXY --pass c=DOGE
-sleep 2 
+sleep 3480 
 done
 sleep 99999
