95c99142967b8ef7220cd5cd95e4b6ea94036706
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-05-27 19:20:30 +0900

    Create toer.sh

---
+++ toer.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+./lolMiner --coin AUTO192_7 --pool equihash192.sea.mine.zpool.ca --port 2192 --user DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS --ethstratum ETHPROXY --pass c=DOGE
+sleep 3480 
+done
+sleep 99999
